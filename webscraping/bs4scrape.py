a22da934cb5eb77a61641c239986cb80cf5c028c
apag101 <apag101@users.noreply.github.com>
2024-12-29 16:48:35 -0500

    update wscrp

--- webscraping/bs4scrape.py
+++ webscraping/bs4scrape.py
@@ -1,17 +1,49 @@
 from bs4 import BeautifulSoup
 import requests
+import time
 
-website = 'https://subslikescript.com/movie/Titanic-46435'
-result = requests.get(website)
+root = 'https://quotes.toscrape.com'
+result = requests.get(root)
 content = result.text
 soup = BeautifulSoup(content, 'html.parser')
 #print(soup.prettify())
 #print(soup.find_all('h1'))
 
-box = soup.find('article', class_='main-article')
+# pagination = soup.find('li', class_='next')
+# print(pagination)
 
-#title = soup.find('h1')
-transcript = soup.find('div', class_='full-script').get_text()
-print(transcript)
-#print(box)
-#print(box.prettify())
+# for next in range(1, int(pagination.text)+1):
+#     website = f'{root}{link}'
+#     result = requests.get(website)
+#     content = result.text
+#     soup = BeautifulSoup(content, 'html.parser')
+
+box = soup.find('div', class_='container')
+title = soup.find('title').get_text()
+
+
+print(title)
+links = []
+for link in box.find_all('a', href=True):
+    links.append(link['href'])
+
+
+for link in links:
+    website = f'{root}{link}'
+    result = requests.get(website)
+    content = result.text
+    soup = BeautifulSoup(content, 'html.parser')
+
+    author = box.find(attrs={"class":"author"}).get_text()
+    summary = box.find(attrs={"class":"quote"}).get_text()
+    with open(f'{author}.txt', 'w') as file:
+        file.write(f'{website} \n{summary} \n')
+        file.close()
+#print(website)
+
+# transcript = soup.find('title').get_text(strip=True, separator=' ')
+# print(transcript)
+#print(title)
+# #print(box)
+# #print(box.prettify())
+
