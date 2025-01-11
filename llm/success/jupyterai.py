69d80c57edaa9f24edbad49eb30a34ba186ed0d9
apag101 <apag101@users.noreply.github.com>
2025-01-10 22:30:04 -0500

    jupyter

---
+++ llm/success/jupyterai.py
@@ -0,0 +1,11 @@
+# medium article: https://medium.com/artificial-corner/jupyter-ai-the-ai-extension-for-jupyter-lab-c4c3be5444fe
+
+# see links for details
+
+
+# $ conda create -n jupyter-ai python=3.10
+# $ conda activate jupyter-ai
+# $ pip install jupyter_ai
+
+# $ pip uninstall grpcio; conda install grpcio
+# jupyter lab
