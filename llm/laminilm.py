c4520f08ec3d6c2e4efde16a98632de676a666b5
apag101 <apag101@users.noreply.github.com>
2025-01-13 22:00:53 -0500

    streamlit

---
+++ llm/laminilm.py
@@ -0,0 +1,5 @@
+# Medium artical https://medium.com/artificial-corner/a-7-steps-guide-to-install-and-run-lamini-lm-on-your-computer-7841ea94522b
+
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import pipeline
+import torch
